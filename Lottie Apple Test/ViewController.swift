cb8d3a44298e2da68d6be70e0565ec20c9119636
Leo Dominguez <ld990y@us.att.com>
2019-03-27 12:23:34 -0700

    Initial Commit

---
+++ Lottie Apple Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lottie Apple Test
+//
+//  Created by DOMINGUEZ, LEO on 3/27/19.
+//  Copyright © 2019 xds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
