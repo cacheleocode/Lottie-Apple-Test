b9c1c050d95ea2bac5c875525c9a6beae110acc1
Leo Dominguez <leo@leodominguez.com>
2019-04-05 07:48:21 -0700

    gitignore

---
+++ Pods/Target Support Files/Pods-Lottie Apple Test/Pods-Lottie Apple Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lottie_Apple_Test : NSObject
+@end
+@implementation PodsDummy_Pods_Lottie_Apple_Test
+@end
---
+++ Pods/Target Support Files/Pods-Lottie Apple TestTests/Pods-Lottie Apple TestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lottie_Apple_TestTests : NSObject
+@end
+@implementation PodsDummy_Pods_Lottie_Apple_TestTests
+@end
